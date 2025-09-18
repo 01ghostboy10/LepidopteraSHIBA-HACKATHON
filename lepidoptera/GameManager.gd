398af2c8fefca0f306887eb5b2e03b49e8cc17f4
01ghostboy10 <levana.rai.simcik@mensa.cz>
2025-09-18 22:47:25 +0200

    FINALLY made the butterfly noise actually play (randomised pitch)

--- lepidoptera/GameManager.gd
+++ lepidoptera/GameManager.gd
@@ -2,8 +2,25 @@ extends Node
 
 var butterflies: int = 0
 var label: Label = null
-
-@onready var sfx_player: AudioStreamPlayer = $butterflynoisess
+#hello world this is   suuposed to play the music notificaiton tone when you or i interatct with w/ a butterfly but apparently that is not the case~
+# why hwyhyh is the godot time more always than the hackatime
+# and would this impact anything ahjdsj
+# becayseee beacuse here is displays the time as 2h37 but in hacktime
+# it is it's it is like something like 1h53 which is objectively
+# unfair why why why why whyw hyw hywhywhwyhwhwhwhwhwwwwhwwhwhywhywhy
+# qwertyuiopasdfghjklzxcvbnm qazwsxedcrfvtgbyhnujmikolp
+# plmoknijbuhvygctfxrdzeswaq what even is this
+# technically I am writign important code
+# important as in I do NOT want *NOT* want to not I DO NOT WANT TO
+# risk not getting the right timings on hackatime and missing out
+# on the randomness randomise week thing aaaaaaa
+# 7 characters of the letter A type were types above beacyyseu bc
+# 7 imo imao is a lucky number
+# wait wait ty to Brody for specifying:
+# that was the dayum ddamn leaderboard timings,
+# not on par with the actualt time ajhdsh
+# anyawyas myehbybeybe
+# hu j
 
 func add_butterfly():	
 	butterflies += 1
