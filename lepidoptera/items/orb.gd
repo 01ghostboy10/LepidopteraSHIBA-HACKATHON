ddb266f5328231128cebca02b57d6af4648bdd96
01ghostboy10 <levana.rai.simcik@mensa.cz>
2025-09-23 13:05:19 +0200

    uhhhh should finally work now. i debuggeed the debug shjdhajhjhj

--- lepidoptera/items/orb.gd
+++ lepidoptera/items/orb.gd
@@ -1,5 +1,29 @@
+#extends Area3D
+#func _ready():
+#	$Area3D.monitoring = false
+#if get_node("../Orb").visible == true:
+#	get_node("../Area3D").monitoring = true
+#func interact():
+#	$"../orbnoise".play()
+#	print("orbnoises are playing")
+#fdddfdfdjahdhajhdjahdjashdjahdjhsaj
+
+
 extends Area3D
 
+func _ready():
+	monitoring = false  # Start disabled
+
+	var orb = get_node_or_null("../Orb")
+	if orb and orb.visible:
+		monitoring = true  # Enable interaction if Orb is visible
+	else:
+		print("Orb is not visible or not found")
+
 func interact():
-	$"../orbnoise".play()
-	print("orbnoises are playing")
+	var orb_noise = get_node_or_null("../orbnoise")
+	if orb_noise:
+		orb_noise.play()
+		print("Orb noise is playing thank god")
+	else:
+		print("orbnoise not found euuggh")
